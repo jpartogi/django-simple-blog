02284e5a061d53df0004c52a445e6a9e526beb4b
Joshua Partogi <joshua.java@gmail.com>
2009-10-04 18:39:49 +1100

    enhance feeds title and description

--- feeds.py
+++ feeds.py
@@ -1,4 +1,5 @@
 # $Id: feeds.py 5ee3537e5395 2009/08/22 11:31:30 jpartogi $
+from django.utils.translation import ugettext as _
 from django.contrib.syndication.feeds import Feed
 from django.contrib.sites.models import Site
 from django.utils.feedgenerator import Atom1Feed
@@ -7,9 +8,17 @@ from djblog.models import Entry, Category
 
 class EntriesFeed(Feed):
     feed_type = Atom1Feed
-    title = "Blog Entries Feed"
-    description = "Blog Entries Feed"
     description_template = 'blog/feed.html'
+
+    def title(self):
+        if not hasattr(self, '_site'):
+            self._site = Site.objects.get_current()
+        return _("%(site_name)s Feed") % dict(site_name=self._site.name)
+
+    def description(self):
+        if not hasattr(self, '_site'):
+            self._site = Site.objects.get_current()
+        return _("Latest blog entries on %(site_name)s") % dict(site_name=self._site.name)
 
     def items(self):
         return Entry.objects.get_latest_posted_entries()[:10]
