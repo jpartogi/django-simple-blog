74c32a5fc6fc0c8192d4a300252ff397e1e1e1a3
Joshua Partogi <jpartogi@vostro.(none)>
2009-11-19 18:10:16 +1100

    added posted in the list grid

--- simple_blog/admin.py
+++ simple_blog/admin.py
@@ -18,7 +18,7 @@ class EntryAdmin(admin.ModelAdmin):
     list_select_related = True
     search_fields = ('title', 'content')
     exclude = ('creator', 'created')
-    list_display = ('title', 'category', 'creator', 'created', 'updated', 'is_draft')
+    list_display = ('title', 'category', 'creator', 'posted', 'created', 'updated', 'is_draft')
     list_filter = ('created', 'category')
     prepopulated_fields = {"slug": ("title",)}
 
